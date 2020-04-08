f013241729037b44af6a6a7b2a1bbdb4541f54fe
Lucas Assis <lucasassistomaz@gmail.com>
2020-04-08 13:30:32 -0300

    first commit (:

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,25 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    try {
+      const { devId } = req.params;
+      const { user } = req.headers;
+
+      const loggedDev = await Dev.findById(user);
+      const targetDev = await Dev.findById(devId);
+
+      if (!targetDev) {
+        return res.status(400).json({ error: "Dev not exists" });
+      }
+
+      loggedDev.dislikes.push(targetDev._id);
+
+      await loggedDev.save();
+
+      return res.json(loggedDev);
+    } catch (err) {
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+  },
+};
